1c48bba9db12f95d3d763b58301b5b0da77bcd95
Denis Makarenko <dmakarenko@wiley.com>
2020-12-14 20:54:35 +0100

    Add filters and export to json

--- sites/all/modules/custom/bank/src/Views/Handlers/Area/BankHandler.php
+++ sites/all/modules/custom/bank/src/Views/Handlers/Area/BankHandler.php
@@ -2,6 +2,10 @@
 
 namespace Drupal\bank\Views\Handlers\Area;
 
+/**
+ * Class BankHandler
+ * @package Drupal\bank\Views\Handlers\Area
+ */
 class BankHandler extends \views_handler_area {
 
     public function render($empty = FALSE)
--- sites/all/modules/russiabank/src/Controller/MainController.php
+++ sites/all/modules/russiabank/src/Controller/MainController.php
@@ -11,7 +11,7 @@ class MainController {
     const XML_DAILY_RATE = 'http://www.cbr.ru/scripts/XML_daily.asp';
     const XML_DINAMIC_RATE = 'http://www.cbr.ru/scripts/XML_dynamic.asp';
 
-    const PATH_FILE = '/var/www/russia_bank/public/';
+    const PATH_FILE = '/var/www/russia_bank_docker/export.json';
 
 
     /**
@@ -27,30 +27,100 @@ class MainController {
      */
     public function getDailyData(string $url = '') : array
     {
+        // get params
+        $params = drupal_get_query_parameters();
+        $paramsCurrencies = isset($params['currencies_options']) ? $params['currencies_options'] : null;
+        $paramsFromDate = isset($params['date_from']) ? $params['date_from'] : null;
+        $paramsToDate = isset($params['date_to']) ? $params['date_to'] : null;
+        $paramsByPage = isset($params['by_page']) ? $params['by_page'] : null;
+        $paramsExport = isset($params['export']) ? $params['export'] : null;
+
+        $mode = false;
+        if (isset($paramsFromDate) && !empty($paramsFromDate)) {
+            $url = self::XML_DAILY_RATE.'?date_req='.$paramsFromDate;
+            $mode = true;
+        }
+
+        if (isset($paramsToDate) && !empty($paramsToDate)) {
+            $url = self::XML_DAILY_RATE.'?date_req='.$paramsToDate;
+            $mode = true;
+        }
+
+        if (isset($paramsFromDate) && !empty($paramsFromDate) && isset($paramsToDate) && !empty($paramsToDate)) {
+            $url = self::XML_DAILY_RATE.'?date_req1='.$paramsFromDate.'&date_req2='.$paramsToDate;
+            $mode = true;
+        }
+
+        if (isset($paramsFromDate) && !empty($paramsFromDate) &&
+            isset($paramsToDate) && !empty($paramsToDate) &&
+            isset($paramsCurrencies) && !empty($paramsCurrencies)
+        ) {
+            $url = self::XML_DINAMIC_RATE.'?date_req1='.$paramsFromDate.'&date_req2='.$paramsToDate.'&VAL_NM_RQ='.$paramsCurrencies;
+            $mode = true;
+        }
+
         if (empty($url)) {
             $url = self::XML_DAILY_RATE.'?date_req='.date('d/m/yy');
         }
 
         $data = $this->getXMLData($url);
         if ($data['status'] === false) {
-            return $data;
+            return [];
         }
 
         $xmlData = $data['data'];
 
-        $resultData = [];
-        foreach ($xmlData as $val) {
-            $resultData[] = [
-                'id' => $val->attributes()->ID->__toString(),
-                'numCode' => $val->children()->NumCode->__toString(),
-                'charCode' => $val->children()->CharCode->__toString(),
-                'nominal' => $val->children()->Nominal->__toString(),
-                'name' => $val->children()->Name->__toString(),
-                'value' => $val->children()->Value->__toString(),
-            ];
+        // find currencies
+        if (isset($paramsCurrencies) && !empty($paramsCurrencies)) {
+            $xmlData = $xmlData->xpath('//*[@ID="'.$paramsCurrencies.'"]');
         }
 
-        return $resultData;
+        // export to json
+        $fileToDownload = '';
+        if (isset($paramsExport) && !empty($paramsExport)) {
+            $json = json_encode($xmlData);
+
+            // clear file
+            file_put_contents(self::PATH_FILE, '');
+
+            /// write to file
+            file_put_contents(self::PATH_FILE, $json);
+
+            if (is_file(self::PATH_FILE)) {
+                header('Content-Description: File Transfer');
+                header('Content-Type: application/octet-stream');
+                header('Expires: 0');
+                header('Cache-Control: must-revalidate');
+                header('Pragma: public');
+                header('Content-Length: ' . filesize(self::PATH_FILE));
+                readfile(self::PATH_FILE);
+            }
+        }
+
+        $resultData = [];
+        if ($mode === false) {
+            foreach ($xmlData as $val) {
+                $resultData[] = [
+                    'id' => $val->attributes()->ID->__toString(),
+                    'numCode' => $val->children()->NumCode->__toString(),
+                    'charCode' => $val->children()->CharCode->__toString(),
+                    'nominal' => $val->children()->Nominal->__toString(),
+                    'name' => $val->children()->Name->__toString(),
+                    'value' => $val->children()->Value->__toString(),
+                ];
+            }
+        } else {
+            foreach ($xmlData as $val) {
+                $resultData[] = [
+                    'id' => $val->children()->attributes()->Id->__toString(),
+                    'date' => $val->children()->attributes()->Date->__toString(),
+                    'nominal' => $val->children()->children()->Nominal->__toString(),
+                    'value' => $val->children()->children()->Value->__toString(),
+                ];
+            }
+        }
+
+        return ['data' => $resultData, 'mode' => $mode];
     }
 
     /**
@@ -71,10 +141,7 @@ class MainController {
 
         $resultData = [0 => '---'];
         foreach ($data['data'] as $val) {
-            $resultData[] = [
-                'name' => $val->children()->Name->__toString(),
-                'id' => $val->attributes()->ID->__toString(),
-            ];
+            $resultData[$val->attributes()->ID->__toString()] = $val->children()->Name->__toString();
         }
 
         return $resultData;
